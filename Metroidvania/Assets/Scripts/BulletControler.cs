1e3b6790e0eb600994a3c1f405ff4dceb1438e1c
TalalAlenezi <76274645+TalalAlenezi@users.noreply.github.com>
2022-01-21 21:17:42 +0300

    me first

---
+++ Metroidvania/Assets/Scripts/BulletControler.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletControler : MonoBehaviour
+{
+    public float bulletSpeed;
+    public Rigidbody2D theRB;
+
+    public Vector2 moveDir;
+
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        theRB.velocity = moveDir * bulletSpeed;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Destroy(gameObject);
+    }
+
+    private void OnBecameInvisible()
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ Metroidvania/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Rigidbody2D theRB;
+
+    public float moveSpeed;
+    public float jumpForce;
+
+    public Transform groundPoint; //the empty checker
+    private bool isOnGround; 
+    public LayerMask whatIsGround; //searches for a layer
+
+    public Animator anim;
+
+    public BulletControler shotToFire;
+    public Transform shotPoint;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //move side ways
+        theRB.velocity = new Vector2(Input.GetAxisRaw("Horizontal")* moveSpeed , theRB.velocity.y);
+
+
+        //handle direction change
+        if(theRB.velocity.x < 0)
+        {
+            transform.localScale = new Vector3(-1f, 1f, 1f);
+        }else if(theRB.velocity.x > 0)
+        {
+            transform.localScale = new Vector3(1f, 1f, 1f);
+        }
+
+
+
+        //checking if on ground
+        isOnGround = Physics2D.OverlapCircle(groundPoint.position, .2f, whatIsGround);
+
+
+        //jumping
+        if (Input.GetButtonDown("Jump") && isOnGround)
+        {
+            theRB.velocity = new Vector2(theRB.velocity.x, jumpForce);
+        }
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Instantiate(shotToFire, shotPoint.position, shotPoint.rotation).moveDir = new Vector2(transform.localScale.x,0f);
+        }
+
+
+
+
+
+
+
+
+
+        anim.SetBool("isOnGround", isOnGround);
+        anim.SetFloat("speed", Mathf.Abs(theRB.velocity.x) );
+    }
+}
